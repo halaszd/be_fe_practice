827756aee0f3d89505fd2eca96347bf419665602
halaszd <david.peter.halasz@gmail.com>
2021-08-06 10:08:27 +0200

    practice

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 const path = require('path')
 const express = require('express');
 const server = express();
+const formidable = require('formidable')
 const port = 3000;
+
+// Create frontend and backend folder
+// index.html, style.js, index.js into frontend
+// Cd backend, 
+// Create server.js
+// init npm
+// npm install express –save
 
 server.use('/public', express.static(path.join("../", "frontend", "public")));
 // ha react lenne a frontend és buildelt verziót szeretnénk importálni:
@@ -16,21 +24,23 @@ server.get('/', (req, res) => {
 // a küldött csomag slice-olva van nem egyben adódik át
 // a feljécben (header): mekkora az adat, hány csomagot vár a másik
 // a bejövő fejlécet meg tudjuk változtatni arra, amire akarjuk
-// így lehetne:
-server.use(function (req, res, next) {
-	req.headers['content-type'] = 'application/json';
-	next();
-  });
-
 // arra készíti fel a szervert, hogy JSON fálj jön
-server.use(express.json());
 // ha nested json jönne
-server.use(express.urlencoded({ extended:true }))
-server.post('/', (req, res) => {
-	// req.header("Content-Type", "application/json")
-	console.log(req.body.firstname)
-	res.send("Welcome");
-})
+server.post('/', (req, res, next) => {
+	const form = formidable({ multiples: true, uploadDir: path.join(__dirname, './upload/') }) ;
+   
+	form.parse(req, (err, fields, files) => {
+	  if (err) {
+		next(err);
+		return;
+	  }
+	  res.json({ fields, files });
+	  console.log(fields);
+	  console.log(files);
+	  const userFields = JSON.parse(fields.userJson);
+	  console.log(userFields.firstname);
+	});
+  });
 
 server.listen(port, () => {
 	console.log(`Example app listening at
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -3,13 +3,29 @@
 // Functions
 
 async function postToClick(e) {
+	e.preventDefault();
+	// a submit azért kellett, hogy a gombnyomásra a form értékeit érhessük el
+	const inputs = e.target.querySelectorAll(".input"), values = {};
+
+	const userFile = document.getElementById("userFile");
+
+	for(const input of inputs){
+		// input.name-et jelent
+		values[`${input.name}`] = input.value;
+	}
+
+	const fd = new FormData();
+	fd.append("userJson", JSON.stringify(values));
+	fd.append('userFile', userFile.files[0], userFile.files[0].name)
+	console.log(fd)
+
 	await fetch('http://localhost:3000/',
 		{
 		method: 'Post',
-		headers: {
-			'Content-Type': 'html/text'
-		},
-		body: "Hello" // JSON.stringify( { title: [newTask] } )
+		// headers: {
+		// 	'Content-Type': 'multipart/form-data'
+		// },
+		body: fd
 	}).then(response => response.text()).then(data => console.log(data));
 }
 
@@ -17,12 +33,21 @@ async function postToClick(e) {
 function _load() {
 	const rootElement = document.getElementById("root");
 
-	rootElement.insertAdjacentHTML("beforeend", `
-	<button id="send">HELLO</button>
+	rootElement.insertAdjacentHTML("beforeend", 
+	`
+		<form> 
+			<input name="firstname" class="input" placeholder="First Name" />
+			<input name="lastname" class="input" placeholder="Last Name" />
+			<input name="email" class="input" type="email" placeholder="Email" />
+			<textarea name="message" class="input" placeholder="message" > </textarea>
+			<div>File: <input multiple type="file" id="userFile" /></div>
+			<button type="submit" id="send">send</button>
+		</form>
 	`)
 
-	document.getElementById("send").addEventListener("click", postToClick)
-	console.log("A frontend betöltődött")
+	// document.getElementById("send").addEventListener("click", postToClick)
+	// console.log("A frontend betöltődött")
+	window.addEventListener("submit", postToClick)
 };
 
 
